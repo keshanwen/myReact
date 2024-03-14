f4328adfc779c09a4a728d5bcfd3a772e2a398cf
keshanwen <3640@winnermedical.com>
2024-03-14 11:42:21 +0800

    feat: React.crelement,ReactDom.render 初实现

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+import React from './react/react';
+// import ReactDOM from 'react-dom';
+import ReactDOM from './react/react-dom';
 
 
-ReactDOM.render(<div>hello wrold</div>, document.getElementById('root'));
+let element1 = (
+  <div className="title" style={{ color: "red" }}>
+    <span>hello</span>, i am kebi
+  </div>
+);
+// console.log(JSON.stringify(element1, null, 2));
+
+
+ReactDOM.render(element1, document.getElementById('root'));
