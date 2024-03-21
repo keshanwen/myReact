bd3998b514470b976c6fc69b3fadb6b8ee7c8798
keshanwen <3640@winnermedical.com>
2024-03-21 10:56:39 +0800

    feat: react 生命周期

--- src/explain.js
+++ src/explain.js
@@ -44,3 +44,316 @@
   Ref 转发允许某些组件接收 ref，并将其向下传递给子组件
 
 */
+
+/* import React from './react';
+import ReactDOM from './react-dom';
+class Counter extends React.Component { // 他会比较两个状态相等就不会刷新视图 PureComponent是浅比较
+  static defaultProps = {
+    name: '珠峰架构'
+  };
+  constructor(props) {
+    super(props);
+    this.state = { number: 0 }
+    console.log('Counter 1.constructor')
+  }
+  componentWillMount() { // 取本地的数据 同步的方式：采用渲染之前获取数据，只渲染一次
+    console.log('Counter 2.componentWillMount');
+  }
+  componentDidMount() {
+    console.log('Counter 4.componentDidMount');
+  }
+  handleClick = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+  // react可以shouldComponentUpdate方法中优化 PureComponent 可以帮我们做这件事
+  shouldComponentUpdate(nextProps, nextState) { // 代表的是下一次的属性 和 下一次的状态
+    console.log('Counter 5.shouldComponentUpdate');
+    return nextState.number % 2 === 0;
+    // return nextState.number!==this.state.number; //如果此函数种返回了false 就不会调用render方法了
+  } //不要随便用setState 可能会死循环
+  componentWillUpdate() {
+    console.log('Counter 6.componentWillUpdate');
+  }
+  componentDidUpdate() {
+    console.log('Counter 7.componentDidUpdate');
+  }
+  render() {
+    console.log('Counter 3.render');
+    return (
+      <div>
+        <p>{this.state.number}</p>
+        {this.state.number === 4 ? null : <ChildCounter count={this.state.number} />}
+        <button onClick={this.handleClick}>+</button>
+      </div>
+    )
+  }
+}
+class ChildCounter extends React.Component {
+  componentWillUnmount() {
+    console.log(' ChildCounter 6.componentWillUnmount')
+  }
+  componentWillMount() {
+    console.log('ChildCounter 1.componentWillMount')
+  }
+  render() {
+    console.log('ChildCounter 2.render')
+    return (<div>
+      {this.props.count}
+    </div>)
+  }
+  componentDidMount() {
+    console.log('ChildCounter 3.componentDidMount')
+  }
+  componentWillReceiveProps(newProps) { // 第一次不会执行，之后属性更新时才会执行
+    console.log('ChildCounter 4.componentWillReceiveProps')
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('ChildCounter 5.shouldComponentUpdate')
+    return nextProps.count % 3 === 0; //子组件判断接收的属性 是否满足更新条件 为true则更新
+  }
+}
+ReactDOM.render(<Counter />, document.getElementById('root')); */
+
+
+/**
+click 1
+Counter 1.constructor
+Counter 2.componentWillMount
+Counter 3.render
+ChildCounter 1.componentWillMount
+ChildCounter 2.render
+ChildCounter 3.componentDidMount
+Counter 4.componentDidMount
+
+click 2
+Counter 5.shouldComponentUpdate
+click 3
+Counter 5.shouldComponentUpdate
+Counter 6.componentWillUpdate
+Counter 3.render
+ChildCounter 4.componentWillReceiveProps
+Counter 5.shouldComponentUpdate
+Counter 7.componentDidUpdate
+
+click3
+Counter 5.shouldComponentUpdate
+
+click4
+Counter 5.shouldComponentUpdate
+Counter 6.componentWillUpdate
+Counter 3.render
+ChildCounter 6.componentWillUnmount
+Counter 7.componentDidUpdate
+
+click5
+Counter 5.shouldComponentUpdate
+
+click6
+Counter 5.shouldComponentUpdate
+Counter 6.componentWillUpdate
+Counter 3.render
+ChildCounter 1.componentWillMount
+ChildCounter 2.render
+ChildCounter 3.componentDidMount
+Counter 7.componentDidUpdate
+
+click7
+Counter 5.shouldComponentUpdate
+
+click8
+Counter 5.shouldComponentUpdate
+Counter 6.componentWillUpdate
+Counter 3.render
+ChildCounter 4.componentWillReceiveProps
+Counter 5.shouldComponentUpdate
+Counter 7.componentDidUpdate
+ */
+
++import { REACT_TEXT, REACT_FORWARD_REF_TYPE } from "./constants";
++import { addEvent } from "./event";
+
+function render(vdom, parentDOM) {
+  let newDOM = createDOM(vdom)
+  if (newDOM) {
+    parentDOM.appendChild(newDOM);
+    if (newDOM._componentDidMount) newDOM._componentDidMount();
+  }
+}
+export function createDOM(vdom) {
+  let { type, props, ref } = vdom;
+  let dom;
+  if (type && type.$$typeof === REACT_FORWARD_REF_TYPE) {
+    return mountForwardComponent(vdom);
+  } else if (type === REACT_TEXT) {
+    dom = document.createTextNode(props);
+  } else if (typeof type === "function") {
+    if (type.isReactComponent) {
+      return mountClassComponent(vdom);
+    } else {
+      return mountFunctionComponent(vdom);
+    }
+  } else {
+    dom = document.createElement(type);
+  }
+  if (props) {
+    updateProps(dom, {}, props);
+    if (typeof props.children == "object" && props.children.type) {
+      mount(props.children, dom);
+    } else if (Array.isArray(props.children)) {
+      reconcileChildren(props.children, dom);
+    }
+  }
+  vdom.dom = dom;
+  if (ref) ref.current = dom;
+  return dom;
+}
+function mountForwardComponent(vdom) {
+  let { type, props, ref } = vdom;
+  let renderVdom = type.render(props, ref);
+  vdom.oldRenderVdom = renderVdom;
+  return createDOM(renderVdom);
+}
+function mountClassComponent(vdom) {
+  let { type, props, ref } = vdom;
+  let classInstance = new type(props);
+  + vdom.classInstance = classInstance;
+  if (ref) ref.current = classInstance;
+  if (classInstance.componentWillMount) classInstance.componentWillMount();
+  let renderVdom = classInstance.render();
+  classInstance.oldRenderVdom = renderVdom;
+  let dom = createDOM(renderVdom);
+  if (classInstance.componentDidMount)
+    dom.componentDidMount = classInstance.componentDidMount.bind(classInstance);
+  return dom;
+}
+function mountFunctionComponent(vdom) {
+  let { type, props } = vdom;
+  let renderVdom = type(props);
+  vdom.oldRenderVdom = renderVdom;
+  return createDOM(renderVdom);
+}
+function updateProps(dom, oldProps = {}, newProps = {}) {
+  for (let key in newProps) {
+    if (key === 'children') {
+      continue;
+    } else if (key === 'style') {
+      let styleObj = newProps[key];
+      for (let attr in styleObj) {
+        dom.style[attr] = styleObj[attr];
+      }
+    } else if (key.startsWith('on')) {
+      addEvent(dom, key.toLocaleLowerCase(), newProps[key]);
+    } else {
+      dom[key] = newProps[key];
+    }
+  }
+  for (let key in oldProps) {
+    if (!newProps.hasOwnProperty(key)) {
+      dom[key] = null;
+    }
+  }
+}
+export function findDOM(vdom) {
+  if (!vdom) return null;
+  if (vdom.dom) {//vdom={type:'h1'}
+    return vdom.dom;
+  } else {
+    let renderVdom = vdom.classInstance ? vdom.classInstance.oldRenderVdom : vdom.oldRenderVdom;
+    return findDOM(renderVdom);
+  }
+}
++function unMountVdom(vdom) {
+  +    let { type, props, ref } = vdom;
+  +    let currentDOM = findDOM(vdom);//获取此虚拟DOM对应的真实DOM
+  +    //vdom可能是原生组件span 类组件 classComponent 也可能是函数组件Function
+    +    if (vdom.classInstance && vdom.classInstance.componentWillUnmount) {
+      +        vdom.classInstance.componentWillUnmount();
+      +    }
+  +    if (ref) {
+    +        ref.current = null;
+    +    }
+  +    //如果此虚拟DOM有子节点的话，递归全部删除
+    +    if (props.children) {
+      +        //得到儿子的数组
+        +        let children = Array.isArray(props.children) ? props.children : [props.children];
+      +        children.forEach(unMountVdom);
+      +    }
+  +    //把自己这个虚拟DOM对应的真实DOM从界面删除
+    +    if (currentDOM) currentDOM.remove();
+  +}
+  +export function compareTwoVdom(parentDOM, oldVdom, newVdom, nextDOM) {
+    +  if (!oldVdom && !newVdom) {
+      +    //老和新都是没有
+        +    return;
+      +  } else if (!!oldVdom && !newVdom) {
+        +    //老有新没有
+          +    unMountVdom(oldVdom);
+        +  } else if (!oldVdom && !!newVdom) {
+          +    //老没有新的有
+            +    let newDOM = createDOM(newVdom);
+          +    if (nextDOM) parentDOM.insertBefore(newDOM, nextDOM);
+          +    else parentDOM.appendChild(newDOM);
+          +    if (newDOM.componentDidMount) newDOM.componentDidMount();
+          +    return;
+          +  } else if (!!oldVdom && !!newVdom && oldVdom.type !== newVdom.type) {
+            +    //新老都有，但类型不同
+              +    let newDOM = createDOM(newVdom);
+            +    unMountVdom(oldVdom);
+            +    if (newDOM.componentDidMount) newDOM.componentDidMount();
+            +  } else {
+      +    updateElement(oldVdom, newVdom);
+      +  }
+    +}
++function updateElement(oldVdom, newVdom) {
+  +    if (oldVdom.type === REACT_TEXT) {
+    +       let currentDOM = newVdom.dom = findDOM(oldVdom);
+    +       if (oldVdom.props !== newVdom.props) {
+      +           currentDOM.textContent = newVdom.props;
+      +       }
+    +       return;
+    +    } else if (typeof oldVdom.type === 'string') {
+      +        let currentDOM = newVdom.dom = findDOM(oldVdom);
+      +        updateProps(currentDOM, oldVdom.props, newVdom.props);
+      +        updateChildren(currentDOM, oldVdom.props.children, newVdom.props.children);
+      +    } else if (typeof oldVdom.type === 'function') {
+        +        if (oldVdom.type.isReactComponent) {
+          +            updateClassComponent(oldVdom, newVdom);
+          +        } else {
+          +            updateFunctionComponent(oldVdom, newVdom);
+          +        }
+        +    }
+  +}
+  + function updateFunctionComponent(oldVdom, newVdom) {
+    +    let currentDOM = findDOM(oldVdom);
+    +    if (!currentDOM) return;
+    +    let parentDOM = currentDOM.parentNode;
+    +    let { type, props } = newVdom;
+    +    let newRenderVdom = type(props);
+    +    compareTwoVdom(parentDOM, oldVdom.oldRenderVdom, newRenderVdom);
+    +    newVdom.oldRenderVdom = newRenderVdom;
+    +}
+  + function updateClassComponent(oldVdom, newVdom) {
+    +    let classInstance = newVdom.classInstance = oldVdom.classInstance;
+    +    if (classInstance.componentWillReceiveProps) {
+      +        classInstance.componentWillReceiveProps(newVdom.props);
+      +    }
+    +    classInstance.updater.emitUpdate(newVdom.props);
+    +}
+  + function updateChildren(parentDOM, oldVChildren, newVChildren) {
+    +    oldVChildren = (Array.isArray(oldVChildren) ? oldVChildren : oldVChildren ? [oldVChildren]).filter(item => item) : [];
+    +    newVChildren = (Array.isArray(newVChildren) ? newVChildren : newVChildren ? [newVChildren]).filter(item => item) : [];
+    +    let maxLength = Math.max(oldVChildren.length, newVChildren.length);
+    +    for (let i = 0; i < maxLength; i++) {
+      +       let nextVdom = oldVChildren.find((item, index) => index > i && item && findDOM(item));
+      +       compareTwoVdom(parentDOM, oldVChildren[i], newVChildren[i], nextVdom && findDOM(nextVdom));
+      +    }
+    +}
+function reconcileChildren(childrenVdom, parentDOM) {
+  for (let i = 0; i < childrenVdom.length; i++) {
+    mount(childrenVdom[i], parentDOM);
+  }
+}
+const ReactDOM = {
+  render,
+};
+export default ReactDOM;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,98 @@
-
+/*
 import React from './react/react';
-import ReactDOM from './react/react-dom';
+import ReactDOM from './react/react-dom'; */
 
-/* import React from 'react';
-import ReactDOM from 'react-dom'; */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+
+class ChildCounter extends React.Component {
+  componentWillMount() {
+    console.log('  ChildCounter 1.componentWillMount')
+  }
+
+  render() {
+    console.log('  ChildCounter 2.render')
+    return (
+      <div>
+        { this.props.count }
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    console.log('  ChildCounter 3.componentDidMount')
+  }
+
+  componentWillReceiveProps(newProps) { // 第一次不会执行，之后属性更新时才会执行
+    console.log('  ChildCounter 4.componentWillReceiveProps', newProps)
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('  ChildCounter 5.shouldComponentUpdate', nextProps.count % 3 === 0)
+    return nextProps.count % 3 === 0 // 子组件判断接收的属性 是否满足更新条件（这里的更新是指调用 render 页面显示的更新）为true 则更新
+  }
+
+  componentWillUnmount() {
+    console.log('  ChildCounter 6.componentWillUnmount')
+  }
+}
 
 class Counter extends React.Component {
+  static defaltProps = {
+    name: '珠峰前端'
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { number: 0 }
+    console.log('Counter 1.constructor')
+  }
+
+  componentWillMount() { // 取本地的数据 同步的方式： 采用渲染之前获取的数据，只渲染一次
+    console.log('Counter 2.componentWillMount');
+  }
+
+  handleClick = () => {
+    this.setState({ number: this.state.number + 1 })
+  }
+
+  render() {
+    console.log('Counter 3.render');
+    return (
+      <div>
+        <p>{this.state.number}</p>
+        {this.state.number === 4 ? null : <ChildCounter count={this.state.number}></ChildCounter>}
+        <button onClick={this.handleClick}>+</button>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    console.log('Counter 4.componentDidMount');
+  }
+
+  // react 可以 shouldComponentUpdate 方法中优化 PureComponent 可以帮我们做这件事
+  shouldComponentUpdate(nextProps,nextState) {
+    console.log('Counter 5.shouldComponentUpdate', nextState.number % 2 === 0);
+    // 如果此函数中返回了 false 就不会调用 render 方法了， 不要随便在这里调用 setState 可能会死循环
+    return nextState.number % 2 === 0
+  }
+
+  componentWillUpdate() {
+    console.log('Counter 6.componentWillUpdate');
+  }
+
+  componentDidUpdate() {
+    console.log('Counter 7.componentDidUpdate');
+  }
+}
+
+ReactDOM.render(<Counter></Counter>, document.getElementById('root'))
+
+/* class Counter extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -54,4 +141,4 @@ class Counter extends React.Component {
 }
 
 
-ReactDOM.render(<Counter></Counter>, document.getElementById('root'))
+ReactDOM.render(<Counter></Counter>, document.getElementById('root')) */
