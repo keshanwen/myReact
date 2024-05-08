7a23a7c4b5f5b2d579eadc8df264f8aaeb27a37e
keshanwen <3640@winnermedical.com>
2024-05-08 10:02:50 +0800

    feat: render props

--- src/demo.js
+++ src/demo.js
@@ -1,68 +1,30 @@
-import { wrapToVdom } from "./utils";
-import { Component } from './Component';
-import { REACT_FORWARD_REF_TYPE, REACT_FRAGMENT, REACT_CONTEXT, REACT_PROVIDER } from './constants';
-function createElement(type, config, children) {
-  let ref;
-  let key;
-  if (config) {
-    delete config.__source;
-    delete config.__self;
-    ref = config.ref;
-    delete config.ref;
-    key = config.key;
-    delete config.key;
+import React from 'react';
+import ReactDOM from 'react-dom';
+class MouseTracker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { x: 0, y: 0 };
   }
-  let props = { ...config };
-  if (arguments.length > 3) {
-    props.children = Array.prototype.slice.call(arguments, 2).map(wrapToVdom);
-  } else {
-    props.children = wrapToVdom(children);
+
+  handleMouseMove = (event) => {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
   }
-  return {
-    type,
-    ref,
-    key,
-    props,
-  };
-}
-function createRef() {
-  return { current: null };
-}
-function forwardRef(render) {
-  var elementType = {
-    $$typeof: REACT_FORWARD_REF_TYPE,
-    render: render
-  };
-  return elementType;
-}
-function createContext() {
-  let context = { $$typeof: REACT_CONTEXT };
-  context.Provider = {
-    $$typeof: REACT_PROVIDER,
-    _context: context
+
+  render() {
+    return (
+      <div onMouseMove={this.handleMouseMove}>
+        {this.props.render(this.state)}
+      </div>
+    );
   }
-  context.Consumer = {
-    $$typeof: REACT_CONTEXT,
-    _context: context
-  }
-  return context;
 }
-+function cloneElement(element, newProps, ...newChildren) {
-  +  let oldChildren = element.props && element.props.children;
-  +  let children = [...(Array.isArray(oldChildren) ? oldChildren : [oldChildren]), ...newChildren]
-    +    .filter(item => item !== undefined)
-    +    .map(wrapToVdom);
-  +  if (children.length === 1) children = children[0];
-  +  let props = { ...element.props, ...newProps, children };
-  +  return { ...element, props };
-  +}
-const React = {
-  createElement,
-  Component,
-  createRef,
-  forwardRef,
-  Fragment: REACT_FRAGMENT,
-  createContext,
-+ cloneElement
-};
-export default React;
+
+ReactDOM.render(< MouseTracker render={params => (
+  <>
+    <h1>移动鼠标!</h1>
+    <p>当前的鼠标位置是 ({params.x}, {params.y})</p>
+  </>
+)} />, document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -5,57 +5,31 @@ import ReactDOM from './react/react-dom';
 /* import React from 'react';
 import ReactDOM from 'react-dom'; */
 
-class Button extends React.Component {
-  state = { name: '张三' }
-
-  componentWillMount() {
-    console.log('Button componentWillMount');
+class MouseTracker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { x: 0, y: 0 };
   }
 
-  componentDidMount() {
-    console.log('Button componentDidMount');
+  handleMouseMove = (event) => {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
   }
 
   render() {
-    console.log('Button render');
-    return <button name={this.state.name}>{this.props.title}</button>
+    return (
+      <div onMouseMove={this.handleMouseMove}>
+        {this.props.render(this.state)}
+      </div>
+    );
   }
 }
 
-const wrapper = OldComponent => {
-  return class NewComponent extends OldComponent {
-    state = { number: 0 }
-    componentWillMount() {
-      console.log('WrapperButton componentWillMount');
-      super.componentWillMount()
-    }
-    componentDidMount() {
-      console.log('WrapperButton componentDidMount');
-      super.componentDidMount()
-    }
-    handleClick = () => {
-      this.setState({
-        number: this.state.number + 1
-      })
-    }
-    render() {
-      console.log('WrapperButton render');
-      let renderElement = super.render()
-      let newProps = {
-        ...renderElement.props,
-        ...this.state,
-        onClick: this.handleClick
-      }
-      return React.cloneElement(
-        renderElement,
-        newProps,
-        this.state.number
-      )
-    }
-  }
-}
-
-let WrappedButton = wrapper(Button)
-
-
-ReactDOM.render(<WrappedButton title='标题'></WrappedButton>, document.getElementById('root'))
+ReactDOM.render(< MouseTracker render={params => (
+  <>
+    <h1>移动鼠标!</h1>
+    <p>当前的鼠标位置是 ({params.x}, {params.y})</p>
+  </>
+)} />, document.getElementById('root'));
