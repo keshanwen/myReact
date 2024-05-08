5b4e2f1b3162938a324d56e16610e802f9e6f645
keshanwen <3640@winnermedical.com>
2024-05-08 08:34:48 +0800

    feat: 高阶组件，属性代理

--- src/index.js
+++ src/index.js
@@ -1,92 +1,38 @@
- import React from './react/react';
-import ReactDOM from './react/react-dom';
+/*  import React from './react/react';
+import ReactDOM from './react/react-dom'; */
 
-/* import React from 'react';
-import ReactDOM from 'react-dom'; */
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-let ThemeContext = React.createContext();
-console.log(ThemeContext, '666666');
-const { Provider, Consumer } = ThemeContext;
-let style = { margin: '5px', padding: '5px' };
-function Title(props) {
-  console.log('Title');
-  return (
-    <Consumer>
-      {
-        (contextValue) => (
-          <div style={{ ...style, border: `5px solid ${contextValue.color}` }}>
-            Title
-          </div>
-        )
+const loading = message => OldComponent => {
+  return class extends React.Component {
+    render() {
+      const state = {
+        show: () => {
+          console.log('show', message)
+        },
+        hide: () => {
+          console.log('hide', message)
+        }
       }
-    </Consumer>
-  )
-}
-class Header extends React.Component {
-  static contextType = ThemeContext
-  render() {
-    console.log('Header');
-    return (
-      <div style={{ ...style, border: `5px solid ${this.context.color}` }}>
-        Header
-        <Title />
-      </div>
-    )
-  }
-}
-function Content() {
-  console.log('Content');
-  return (
-    <Consumer>
-      {
-        (contextValue) => (
-          <div style={{ ...style, border: `5px solid ${contextValue.color}` }}>
-            Content
-            <button style={{ color: 'red' }} onClick={() => contextValue.changeColor('red')}>变红</button>
-            <button style={{ color: 'green' }} onClick={() => contextValue.changeColor('green')}>变绿</button>
-          </div>
-        )
-      }
-    </Consumer>
-  )
-}
-class Main extends React.Component {
-  static contextType = ThemeContext
-  render() {
-    console.log('Main');
-    return (
-      <div style={{ ...style, border: `5px solid ${this.context.color}` }}>
-        Main
-        <Content />
-      </div>
-    )
+
+      return (
+        <OldComponent {...this.props} {...state} {...{...this.props, ...state }}></OldComponent>
+      )
+    }
   }
 }
 
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { color: 'black' };
-  }
-  changeColor = (color) => {
-    this.setState({ color });
-  }
+@loading('消息')
+class Hello extends React.Component {
   render() {
-
-    console.log('Page');
-    let contextValue = { color: this.state.color, changeColor: this.changeColor };
-    return (
-      <Provider value={contextValue}>
-        <div style={{ ...style, width: '250px', border: `5px solid ${this.state.color}` }}>
-          Page
-          <Header />
-          <Main />
-        </div>
-      </Provider >
-    )
+    return <div>hello <button onClick={this.props.show}>show</button><button onClick={this.props.hide}>hide</button></div>
   }
 }
+
+let LoadingHello = loading('消息')(Hello)
+
+
 ReactDOM.render(
-  <Page />,
-  document.getElementById('root')
-);
+  <LoadingHello></LoadingHello>, document.getElementById('root')
+)
